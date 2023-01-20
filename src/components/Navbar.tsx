69414a42aed75c503e567f4b82e74dfb6dab8f58
Hardi Mahendra <41864461+hardimahendra@users.noreply.github.com>
2023-01-20 15:23:31 +0700

    Update Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import {Link} from 'react-router-dom'
 export function Navbar(){
     return(
         <>
@@ -19,9 +20,9 @@ export function Navbar(){
                     <i id="close" className="bi bi-x-diamond-fill"></i>
                 </label>
                 <ul>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/experience">Experience</a></li>
-                    <li><a href="/projects">Projects</a></li>
+                    <li><a><Link to="/about">About</Link></a></li>
+                    <li><a><Link to="/experience">Experience</Link></a></li>
+                    <li><a><Link to="/projects">Projects</Link></a></li>
                     {/* <li><a href="/contacts">Contacts</a></li> */}
                 </ul>
             </nav>
